65b799e4c1727f9805d58c9e2bc1b88c632f9f89
vinaybetabulls <bodigevinay92@gmail.com>
2019-12-18 18:36:43 +0530

    swagger update

--- index.ts
+++ index.ts
@@ -4,12 +4,15 @@ import * as swaggerUi from "swagger-ui-express";
 import MongoDbConnection from "./src/lib/mongo";
 import jsyml from 'js-yaml'
 import { pathToFileURL } from "url";
-import path from 'path'
+import path from 'path';
+import bodyParser from "body-parser";
+const fs = require('fs');
 
 require('dotenv-safe').config({
-  example: process.env.CI ? '.env.ci.example' : '.env.example'
+  example:  '.env.example'
 });
-const swaggerDoc = jsyml.safeLoad(`${process.cwd}/swagger.json`);
+const swaggerInfo = fs.readFileSync(`${process.cwd()}/src/swagger.json`,'utf-8')
+const swaggerDoc = jsyml.safeLoad(swaggerInfo);
 const swaggerRouterOption = {
   controllers: path.join(__dirname,'/src/controllers')
 }
@@ -18,11 +21,12 @@ const app: any = express();
 let mongodbURL = process.env.MONGO_URI  || 'monodb://localhost:27017/';
 let dbName = process.env.DB_NAME || 'test';
 mongo.init(mongodbURL, dbName);
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({limit:'1000mb',extended:true}))
 swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
   app.use(middleware.swaggerMetadata())
   app.use(middleware.swaggerRouter(swaggerRouterOption))
-  // todo
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('./src/swagger.json'),undefined,undefined,undefined,undefined,undefined,'test'))
 })
 
-app.use
+app.listen(4500)
---
+++ src/controllers/products.ts
@@ -0,0 +1,8 @@
+module.exports.testController = async (req: any, res: any) => {
+    try {
+        res.send({msg: "test"})
+        
+    } catch (error) {
+        
+    }
+}
